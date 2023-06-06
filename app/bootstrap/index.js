2aada9ae3350be9b986da49e1f04f556bfc0e16b
shaqayq <shaqayq.darwazi@gmail.com>
2023-06-06 20:40:38 +0430

    config handelbars

--- app/bootstrap/index.js
+++ app/bootstrap/index.js
@@ -1,11 +1,10 @@
-const exphbs = require('express-handlebars')
-import { engine } from 'express-handlebars';
+
+const exhbs= require('express-handlebars')
 const path = require('path')
 module.exports = app =>{
-    
-
-    app.engine('handlebars', engine());
-    // app.set('view engine', 'handlebars');
-    app.set('views' , path.join(__dirname , '../views'))
-    
+   
+    app.engine('handlebars' , exhbs.engine());
+    app.set('view engine', 'handlebars');
+    app.set('views', path.join(__dirname,'../views'))
+  
 }
--- app/index.js
+++ app/index.js
@@ -2,9 +2,15 @@ const express = require('express')
 const app = express()
 
 require('./bootstrap')(app)
+
+
+app.get('/', function (req, res) {
+  res.render('main', { layout: false, /* additional data */ });
+});
+
 module.exports =() =>{
         const port = process.env.APP_PORT;
-        app.listen(()=>{
+        app.listen(port, ()=>{
             console.log(`Application work in port ${port}`);
         })
 }
