c9b5c5702b60aa89d8adbc219fa9eef98c00d049
shaqayq <shaqayq.darwazi@gmail.com>
2023-06-07 15:25:42 +0430

    cretae totalPosts methods

--- app/controller/adminController.js
+++ app/controller/adminController.js
@@ -3,6 +3,7 @@ const statistics = require('../models/statistics')
 exports.index = async (req,res) => {
     const data ={
         totalUser: await statistics.totalUsers(),
+        totalPost: await statistics.totalPosts()
     }
     res.render('dashboard', { layout: 'main' , ...data});
 }
--- app/models/statistics.js
+++ app/models/statistics.js
@@ -2,6 +2,11 @@ const db = require('../../database/mysql')
 
 exports.totalUsers = async(req , res) => {
     const [users] = await db.query('SELECT COUNT(ID) as totalUser FROM users');
-    console.log(users);
+
     return users[0].totalUser;
+}
+exports.totalPosts = async(req , res) => {
+    const [posts] = await db.query('SELECT COUNT(ID) as totalpost FROM posts');
+
+    return posts[0].totalpost;
 }
