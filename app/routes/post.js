b4766acf646c45b0060d60f893f9e09142bd1893
shaqayq <shaqayq.darwazi@gmail.com>
2023-06-07 16:38:59 +0430

    create postController

--- app/routes/post.js
+++ app/routes/post.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
 const postController = require('')
-router.get('/dashboard', postController.index)
+router.get('/', postController.index)
 
 module.exports = router
