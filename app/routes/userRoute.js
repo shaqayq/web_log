6ed992187140a4f44bb86ce904846b1f1044e31a
shaqayq <shaqayq.darwazi@gmail.com>
2023-06-11 13:27:29 +0430

    create user routs

---
+++ app/routes/userRoute.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router();
+const userController = require('../controller/userController')
+router.get('/', userController.index)
+// router.get('/create' , userController.create)
+// router.post('/store' , userController.store)
+// router.get('/delete/:userId' , userController.deleteUser)
+// router.get('/editUser' , userController.findUser)
+// router.post('/update/:userId' , userController.updateUser)
+
+
+
+
+module.exports = router
