8102b49e0923199bc9b083571f1c7b1a65bd1623
shaqayq <shaqayq.darwazi@gmail.com>
2023-06-13 14:29:21 +0430

    add isChecked function for seeting check box

--- app/controller/settingController.js
+++ app/controller/settingController.js
@@ -6,20 +6,25 @@ const settingModel = require('../models/settingModel')
 exports.index =async(req, res) => {
 
     const settings =await settingModel.getAll();
-   const allsetting= settings.map(setting=>({
-    ...setting, 
-    created_at: moment(setting.created_at).format('YYYY-MM-DD')
-   }))
-    res.render('setting/', {layout: 'main' , allsetting});
+    const allSetting ={}
+    settings.forEach(item=>{
+        allSetting[item.setting_name] = item.setting_value
+    })
+  
+    res.render('setting/', {layout: 'main' , config: allSetting , helpers: {
+        ischecked: function(value, options) {
+            return parseInt(value) === 1 ? options.fn(this) : options.inverse(this);
+        }
+    }});
 }
 
 
 exports.store = async (req, res) => {
 
-const settings = req.body;
+    const settings = req.body;
     const validatingSetting  = {};
     Object.keys(settings).forEach(setting_name => {
-        if(settings[setting_name]=== 'on'){
+        if(settings[setting_name] === 'on'){
             validatingSetting[setting_name] = 1;
         }else{
             validatingSetting[setting_name] = settings[setting_name]
