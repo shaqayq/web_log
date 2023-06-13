b8f7be800e40af2116db1ca575eeca0ff131731e
shaqayq <shaqayq.darwazi@gmail.com>
2023-06-13 15:59:52 +0430

    show default setting in setting page

--- app/controller/settingController.js
+++ app/controller/settingController.js
@@ -1,12 +1,13 @@
 const { validationResult  , body} = require('express-validator');
 const moment = require('moment')
 const settingModel = require('../models/settingModel')
-
+const definedSetting= require('../config/setting')
 
 exports.index =async(req, res) => {
 
     const settings =await settingModel.getAll();
     const allSetting ={}
+
     settings.forEach(item=>{
         allSetting[item.setting_name] = item.setting_value
     })
@@ -22,14 +23,18 @@ exports.index =async(req, res) => {
 exports.store = async (req, res) => {
 
     const settings = req.body;
-    const validatingSetting  = {};
-    Object.keys(settings).forEach(setting_name => {
-        if(settings[setting_name] === 'on'){
-            validatingSetting[setting_name] = 1;
-        }else{
-            validatingSetting[setting_name] = settings[setting_name]
-        }
+    
+    const premittdSettingKeys =Object.keys(settings).filter(setting =>{
+        return Object.keys(definedSetting).includes(setting);
     })
+
+    const premittdSettings = {}
+    premittdSettingKeys.forEach(key=>{
+        premittdSettings[key] = settings[key]
+    })
+
+    const validatingSetting ={...definedSetting , ... premittdSettings}
+
     const result = await settingModel.update(validatingSetting);
     return res.redirect("/setting")
    
