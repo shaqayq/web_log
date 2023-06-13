f421ab4422d715d02de7cf411839f14e92727dc8
shaqayq <shaqayq.darwazi@gmail.com>
2023-06-13 15:44:09 +0430

    create setting config file

---
+++ app/config/setting.js
@@ -0,0 +1,7 @@
+modeul.exports ={
+    websit_title: "Express_Web_Blog",
+    content_per_page: 10 , 
+    details: "Web log for store and  manage posts",
+    user_can_register: 0 ,
+    user_can_submit_comme: 0
+}
