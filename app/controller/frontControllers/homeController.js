c46b724b5592570e1b7a2ed3065fa16d0e29199c
shaqayq <shaqayq.darwazi@gmail.com>
2023-06-18 20:48:42 +0430

    show posts in home page

--- app/controller/frontControllers/homeController.js
+++ app/controller/frontControllers/homeController.js
@@ -1,3 +1,14 @@
-exports.index = (req , res) =>{
-    res.render('front/home' , {layout: false}) 
+const postModel = require('../../models/postModel')
+const moment = require('moment')
+const word_limit = require('../../presenter/post_present')
+
+exports.index = async(req , res) =>{
+    const all_post =await postModel.getAll();
+    const posts = all_post.map(post =>( {
+        ...post , 
+        content: word_limit.excerpt(post.content) ,
+        created_at:  moment(post.created_at).format('YYYY-MM-DD'),
+        }))
+ 
+    res.render('front/home' , {layout: false , posts}) 
 }
