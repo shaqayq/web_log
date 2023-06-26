f1b285177f007179a488387454f126ae8c3c5f83
shaqayq <shaqayq.darwazi@gmail.com>
2023-06-26 16:36:45 +0430

    change Remote DB

--- app/bootstrap/index.js
+++ app/bootstrap/index.js
@@ -11,6 +11,7 @@ const mysqlOptions = {
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USERNAME,
   database: process.env.MYSQL_DATABASE,
+  password: process.env.MYSQL_PASSWORD,
   port: process.env.MYSQL_PORT,
   multipleStatements: true
 }
